dc8082dee77ec12ea20678e792ab36ca4a0b5102
mudit999 <muditgarg1211@gmail.com>
2023-01-17 19:39:22 +0530

    Laying the foundation

--- app.js
+++ app.js
@@ -1,65 +1,39 @@
-/***
- * 
- * Reference links
- * https://parceljs.org/features/development/
- * https://www.npmjs.com/package/browserslist
- * https://browserslist.dev/?q=bGFzdCAyIHZlcnNpb25z
- * https://github.com/browserslist/browserslist#query-composition
- * 
-*/
-
-/***
- * 
- * Parcel provide us
- * 
- * Created server for us
- * HMR - Hot Module Reloading
- * File Watcher Algorithm - C++
- * Bundling
- * Minify / Minification on build environment
- * Cleaning our code
- * Dev $ production build
- * Super Fast build algorithm
- * Image optimization
- * Caching while development
- * Compression
- * Compatible with older version of browser
- * HTTPS on dev
- * Port number
- * Consistent Hashing algorithm
- * Zero config
- * 
- */
-
-/****
- * Transitive dependency
- * browserslist 
- */
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", {}, "heading 1");
-const heading2 = React.createElement("h1", {}, "heading 2");
-const div1 = React.createElement("div", {hello : "World"}, [heading1, heading2]);
-//                                                          Children - heading1, heading~2
+const heading2 = React.createElement(
+  "h1",
+  {
+    key: "h1",
+  },
+  "heading 1"
+);
 
-// creating element with React -> h1
-const heading = React.createElement("h1", {style :{
-    backgroundColor : "green",
-    color: "white",
-},
-    className : "hello"
-}, "Namaste from React.js");
-// {} all the tag attributes from here 
+const heading = <h1>Hello, this is a React element</h1>;
 
-console.log(heading); // It is an Object
+// React components
+// - Functional components - NEW
+// - Class Based components - OLD
 
-// create root element and passing the root
-// we are injecting react inside root
+const HeaderComponent = () => {
+  return (
+    <div>
+      {/* {heading} */}
+      {/* <HeaderComponent2 /> */}
+      {HeaderComponent2()}
+      <h2>Namaste React functional components </h2>
+    </div>
+  );
+};
+
+const HeaderComponent2 = () => (
+  <h1>Namaste React functional components HeaderComponent2</h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// render: modifying the DOM
-// passing a react element inside the root
-// render method is basically injecting heading inside DOM
-root.render([div1,heading]);
+//  rendering react element
+// root.render(heading);
+
+// rendering Functional components
+root.render(<HeaderComponent />);
